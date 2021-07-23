e4101b7722df8ed97ce24e08086f8b74986cd226
Gnelf <henry.lunger@gmail.com>
2021-07-23 14:13:27 -0400

    test

--- bot.js
+++ bot.js
@@ -19,7 +19,7 @@ fs.readdir('./events/', (err, files) => {
 
 //const botToken = process.env.BOT_TOKEN;
 
-client.login('ODUxNTY5NjIxMTA2MDMyNjUx.YL6MAw.mzJMnURIfN3EBWQhGRlvTJpXTRM');
+client.login('');
 
 //TODO LIST
 //#1: trophies command does something if you have trophies (DONE)
